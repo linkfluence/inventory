9096b21ac57e43cc1a13176119501179ec40471e
Jean-Baptiste Besselat <jean-baptiste.besselat@adotmob.com>
2019-08-07 10:10:39 +0200

    [release] 0.16.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject linkfluence/inventory "0.15.5-SNAPSHOT"
+(defproject linkfluence/inventory "0.16.0"
   :description "Rtgi inventory App"
   :url "http://www.linkfluence.com"
   :license {:name "Eclipse Public License"
