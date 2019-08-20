8f6befb28e0f7394e897e4ffa5045b43dbc3f5ba
Jean-Baptiste Besselat <jean-baptiste.besselat@adotmob.com>
2019-08-20 14:29:57 +0200

    [release] v0.16.1 : consul mirrors

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject linkfluence/inventory "0.16.1-SNAPSHOT"
+(defproject linkfluence/inventory "0.16.1"
   :description "Rtgi inventory App"
   :url "http://www.linkfluence.com"
   :license {:name "Eclipse Public License"
