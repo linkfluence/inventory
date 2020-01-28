9518b8aab322f9a01e01c43b8562ee6381957a64
Jean-Baptiste Besselat <jean-baptiste.besselat@adotmob.com>
2020-01-28 18:24:32 +0100

    [gcs] wip

---
+++ src/clj/com/linkfluence/inventory/gcs/compute.clj
@@ -0,0 +1,10 @@
+(ns com.linkfluence.inventory.gcs.compute)
+
+; @author Jean-Baptiste Besselat
+; @Copyright Linkfluence SAS 2020 / Adot 2020
+
+(defn load-inventory! [])
+
+(defn start-inventory-update-consumer! [])
+
+(defn start-loop! [])
