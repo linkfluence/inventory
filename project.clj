5411a62efde4c58280665393774958e849383d87
Jean-Baptiste Besselat <luhhujbb@aol.com>
2020-08-25 12:00:46 +0200

    [snapshot]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject linkfluence/inventory "0.16.7"
+(defproject linkfluence/inventory "0.16.8-SNAPSHOT"
   :description "Rtgi inventory App"
   :url "http://www.linkfluence.com"
   :license {:name "Eclipse Public License"
