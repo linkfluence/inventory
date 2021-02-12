6e5e829fd08159b33c0d3e502b10ed6002643f49
Jean-Baptiste Besselat <jean-baptiste.besselat@adotmob.com>
2021-02-12 09:13:32 +0100

    Dockerfile : 0.16.11 + snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject linkfluence/inventory "0.16.11"
+(defproject linkfluence/inventory "0.16.12-SNAPSHOT"
   :description "Rtgi inventory App"
   :url "http://www.linkfluence.com"
   :license {:name "Eclipse Public License"
